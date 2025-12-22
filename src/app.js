ece0fdd3cca864ebc8c837bde903165346ce30de
AbdulWahid419 <ranaw8537@gmail.com>
2025-12-23 02:30:19 +0500

    updated

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
-const express = require("express")
 const cors = require("cors")
 
-const app = express()
+const allowedOrigins = [
+  "http://localhost:5173/",
+  "https://lacarta-two.vercel.app/"
+]
 
-app.use(cors())
-app.use(express.json())
-
-app.get("/", (req, res) => {
-  res.json({ message: "App is ready" })
-})
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error("Not allowed by CORS"))
+      }
+    },
+    credentials: true
+  })
+)
 
 module.exports = app
